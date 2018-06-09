c9ef4a560a3f6b7beb1161947aaf9f4d89554ebb
Murp01 <pmurphy35@hotmail.com>
2018-06-09 12:14:06 +0100

    Added methods to the step defs
    Copied the methods from the test file.  Test passes successfully.  I
    noticed that when the test is ran that it wasn't logging out.  So I
    tried adding the xpath from the other logout button and it worked.  THe
    issue with using driver.quit still occurs but doesn't affect the test.
    Something to do with streams?  See bug for further info

--- src/stepDefinition/Test_Steps.java
+++ src/stepDefinition/Test_Steps.java
@@ -1,44 +1,52 @@
 package stepDefinition;
 
-import cucumber.api.PendingException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class Test_Steps {
+	
+	private static WebDriver driver = null;
+	
 	@Given("^the user is on the homepage$")
 	public void the_user_is_on_the_homepage() throws Throwable {
-	   
-	    throw new PendingException();
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get("http://www.store.demoqa.com");
 	}
 
 	@When("^navigating to the login page$")
 	public void navigating_to_the_login_page() throws Throwable {
-
-		throw new PendingException();
+        driver.findElement(By.xpath(".//*[@id='account']/a")).click();
 	}
 
 	@When("^the user enters the login name and password$")
 	public void the_user_enters_the_login_name_and_password() throws Throwable {
-
-		throw new PendingException();
+        driver.findElement(By.id("log")).sendKeys("test123321");
+        driver.findElement(By.id("pwd")).sendKeys("welcome1");
+        driver.findElement(By.id("login")).click();
 	}
 
 	@Then("^login successful will be displayed$")
 	public void login_successful_will_be_displayed() throws Throwable {
-
-		throw new PendingException();
+		System.out.println("Login Successfully");
 	}
 
 	@When("^the user logs out of the application$")
 	public void the_user_logs_out_of_the_application() throws Throwable {
-
-		throw new PendingException();
+		//driver.findElement (By.xpath("//*[@id='account_logout']/a")).click();
+		driver.findElement(By.xpath("//*[@id='meta']/ul/li[2]/a")).click();
 	}
 
 	@Then("^logout successful will be displayed$")
 	public void logout_successful_will_be_displayed() throws Throwable {
-
-		throw new PendingException();
+		System.out.println("LogOut Successfully");
+		driver.quit(); 
 	}
 }
