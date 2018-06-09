e5bb8effe7ee2db382c2827c36074edba40579b8
Murp01 <pmurphy35@hotmail.com>
2018-06-09 11:24:29 +0100

    Created step def class Test Steps and added missing step defs
    Copied the missing step defs from Console after attempting to run
    TestRunner.

---
+++ src/stepDefinition/Test_Steps.java
@@ -0,0 +1,44 @@
+package stepDefinition;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Test_Steps {
+	@Given("^the user is on the homepage$")
+	public void the_user_is_on_the_homepage() throws Throwable {
+	   
+	    throw new PendingException();
+	}
+
+	@When("^navigating to the login page$")
+	public void navigating_to_the_login_page() throws Throwable {
+
+		throw new PendingException();
+	}
+
+	@When("^the user enters the login name and password$")
+	public void the_user_enters_the_login_name_and_password() throws Throwable {
+
+		throw new PendingException();
+	}
+
+	@Then("^login successful will be displayed$")
+	public void login_successful_will_be_displayed() throws Throwable {
+
+		throw new PendingException();
+	}
+
+	@When("^the user logs out of the application$")
+	public void the_user_logs_out_of_the_application() throws Throwable {
+
+		throw new PendingException();
+	}
+
+	@Then("^logout successful will be displayed$")
+	public void logout_successful_will_be_displayed() throws Throwable {
+
+		throw new PendingException();
+	}
+}
